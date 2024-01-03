da742b7d71ced7df538459c6bc9b6196385b5e87
hassanzahid@gmail.com <your.email@example.com>
2024-01-03 16:45:21 +0500

    multiplicatin table

--- fictorial.py
+++ fictorial.py
@@ -1,7 +1,7 @@
 #program to generate the factorial of the number:
-
-number = int (input ("Enter the number"))
 factorial=1;
+number = int (input ("Enter the number"))
+
 if number==0:
   print("Factorial of 0 is   1")
 
@@ -10,9 +10,15 @@ elif number< 0 :
 
 else:
 
-    for i in range(1, number+1 ):
+  for i in range(1, number+1 ):
      factorial= factorial*i
-    print("Factorial")
-
-# n * factorial(n - 1)
-
+     
+  print("The Factorial of give number is"  ,factorial)
+    
+    #4
+    #1, 4
+    #1,1
+    #1,2
+   # 1,3
+   # 1,4
+    #
---
+++ multiplicationtable.py
@@ -0,0 +1,10 @@
+# num= int(input("Enter  the number"))
+
+# for i in range(1,11):
+#     print(num,"*",i, "=" ,num)
+      
+nm= int(input("Enter  the number"))
+i=1
+while(i<11):
+    print(nm,"x",i,"=",nm*i)
+    i=i+1;
